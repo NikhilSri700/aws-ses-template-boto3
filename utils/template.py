62f12455f5c5e4d6e60a33fee509852e3ea2be7f
Nikhil Srivastav <44522355+NikhilSri700@users.noreply.github.com>
2022-06-14 13:38:41 +0530

    added update template function

--- utils/template.py
+++ utils/template.py
@@ -31,6 +31,27 @@ class Template:
         else:
             print("Template Created Successfully")
 
+    # Update Email Template
+    @staticmethod
+    def update(template_name, subject, text, html):
+        try:
+            Template.ses.create_template(
+                Template={
+                    'TemplateName': template_name,
+                    'SubjectPart': subject,
+                    'TextPart': text,
+                    'HtmlPart': html
+                }
+            )
+        except Template.ses.exceptions.TemplateDoesNotExistException:
+            print('Template does not exist')
+        except Template.ses.exceptions.InvalidTemplateException:
+            print("Invalid Template")
+        except botocore.exceptions.ParamValidationError:
+            print("Invalid Parameters passed")
+        else:
+            print("Template Updated Successfully")
+
     @staticmethod
     def list_all():
         print(Template.ses.list_templates())
