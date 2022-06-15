20f27125e327895c54332c2897425806c30dbf08
Nikhil Srivastav <44522355+NikhilSri700@users.noreply.github.com>
2022-06-15 15:47:08 +0530

    created templates from main

--- main.py
+++ main.py
@@ -5,25 +5,12 @@ Main python file for the project.
 from utils.email import Email
 from utils.template import Template
 
-
-def mail_body():
-    with open("./templates/text-part.txt", "r") as text_file:
-        text_data = text_file.read()
-    with open("./templates/html-part.html", "r") as html_file:
-        html_data = html_file.read()
-    return text_data, html_data
-
-
 # Program execution will start from here
 if __name__ == '__main__':
-    text, html = mail_body()
-
-    Email.verify_identity('nikhil.srivastav@watchguard.com')
-
-    Template.create('Generic Github MFA', 'Enable MFA', text, html)
+    Template.create_or_update()
     # Template.view('Generic Github MFA')
 
-    # print(Template.list_all())
+    print(Template.list_all())
 
     # Template.delete('Happy')
 
