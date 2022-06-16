c3d8964a35d50026806fb3600031505fa93bd7b0
Nikhil Srivastav <44522355+NikhilSri700@users.noreply.github.com>
2022-06-16 17:09:35 +0530

    Completed send template and bulk mail functionality.

--- utils/email.py
+++ utils/email.py
@@ -5,16 +5,18 @@ Module for all the operations related to email.
 import boto3
 import botocore
 import re
+from utils.template import Template
+import json
 
 
-class Email:
+class Email(Template):
     # AWS SES client created using the default credentials configured for AWS CLI
     ses = boto3.client('ses')
 
     @staticmethod
     def check(email):
-        regex = '^[a-z0-9A-Z]+[\\._]?[A-Za-z0-9]+[@]\\w+[.]\\w{2,3}$'
-        if re.search(regex, email):
+        regex = '[a-zA-Z]+[A-Za-z0-9._%+-]+[a-zA-Z0-9]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}'
+        if re.match(regex, email):
             return True
         else:
             return False
@@ -37,3 +39,56 @@ class Email:
                 print("Verification email sent, Please Verify")
         else:
             print("Email Address is not Valid")
+
+    @staticmethod
+    def send_using_template(source_email, bulk=False):
+        """
+        Static function that will send templated emails using the already created templates with replacement data.
+        :param source_email: str
+            Email address using which mails will be sent.
+        :param bulk: bool
+            Boolean parameter that will be responsible for choice between sending bulk emails or not.
+        :return: None
+        """
+
+        Email.load_jsons()
+        template_list = Template.list_all()
+        counter = 1
+        destination_list = []
+        for destination in Email.destinations:
+            destination_list.append({"Destination": destination,
+                                     "ReplacementTemplateData": str(json.dumps(Email.user_data[counter - 1]))
+                                     })
+            counter += 1
+        template_choice = int(input("Choose one template: "))
+
+        if bulk:
+            try:
+                Email.ses.send_bulk_templated_email(
+                    Source=source_email,
+                    DefaultTemplateData='{"name": "test", "username": "test", "data": "Unknown"}',
+                    Destinations=destination_list,
+                    Template=template_list[template_choice-1],)
+            except Email.ses.exceptions.MessageRejected:
+                print("Message Rejected")
+            except Email.ses.exceptions.MailFromDomainNotVerifiedException:
+                print("Email address is not verified, Verify using option 1 from Menu.")
+            except Exception as error:
+                print(f"Exception Occurred: {error}")
+            else:
+                print("All mails sent successfully")
+        else:
+            try:
+                Email.ses.send_templated_email(
+                    Source=source_email,
+                    Destination=Email.destinations[0],
+                    Template=template_list[template_choice-1],
+                    TemplateData=str(json.dumps(Email.user_data[0])))
+            except Email.ses.exceptions.MessageRejected:
+                print("Message Rejected")
+            except Email.ses.exceptions.MailFromDomainNotVerifiedException:
+                print("Email address is not verified, Verify using option 1 from Menu.")
+            except Exception as error:
+                print(f"Exception Occurred: {error}")
+            else:
+                print("Mail sent successfully")
