d2ff246cbeedebf254cf59a718a480ac55ba86b0
Nikhil Srivastav <44522355+NikhilSri700@users.noreply.github.com>
2022-06-21 01:19:18 +0530

    changed prints to return statements.

--- utils/template.py
+++ utils/template.py
@@ -12,26 +12,26 @@ class Template(Config):
     ses = boto3.client('ses')
 
     @staticmethod
-    def create_or_update():
+    def create_or_update(template_json='config/config_template.json'):
         """
         Static function that is used to create or update all the templates stored in 'config_template.json'.
         It will iterate through all the templates one by one, and if that template is new then it will create that
         template, and if it already exists then it will update that template.
-        :return: None
+        :return: str
         """
-        Template.load_template_json()
+        all_templates = Template.load_json(template_json)
 
-        for templateId, template in Template.all_templates.items():
+        for templateId, template in all_templates.items():
             try:
                 Template.ses.create_template(Template=template)
-                print(f"Template \'{template['TemplateName']}\' Created Successfully")
+                return f"Template \'{template['TemplateName']}\' Created Successfully"
             except Template.ses.exceptions.AlreadyExistsException:
                 Template.ses.update_template(Template=template)
-                print(f"Template \'{template['TemplateName']}\' already exist, updated successfully")
+                return f"Template \'{template['TemplateName']}\' already exist, updated successfully"
             except Template.ses.exceptions.InvalidTemplateException:
-                print(f"Invalid Template: {templateId}")
+                return f"Invalid Template: {templateId}"
             except botocore.exceptions.ParamValidationError:
-                print(f"Invalid Parameters passed for {templateId}")
+                return f"Invalid Parameters passed for {templateId}"
 
     @staticmethod
     def delete(template_name):
@@ -39,14 +39,14 @@ class Template(Config):
         Static function that will delete a template.
         :param template_name: str
             Name of the template that will be deleted.
-        :return: None
+        :return: str
         """
         try:
             Template.ses.delete_template(TemplateName=template_name)
         except Exception as error:
-            print(f'Exception occurred: {error}')
+            return f'Exception occurred: {error}'
         else:
-            print('Template deleted successfully')
+            return 'Template deleted successfully'
 
     @staticmethod
     def view(template_name):
@@ -54,12 +54,12 @@ class Template(Config):
         Static function that will give details about a template.
         :param template_name: str
             Name of the template that will be described.
-        :return: None
+        :return: str
         """
         try:
-            print(Template.ses.get_template(TemplateName=template_name))
+            return Template.ses.get_template(TemplateName=template_name)
         except Template.ses.exceptions.TemplateDoesNotExistException:
-            print('Template does not exist')
+            return 'Template does not exist'
 
     @staticmethod
     def list_all():
