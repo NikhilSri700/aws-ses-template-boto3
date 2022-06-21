355bc82619b4239a4fb7040d93e5d9f251aff415
Nikhil Srivastav <44522355+NikhilSri700@users.noreply.github.com>
2022-06-21 16:31:16 +0530

    wrote tests for all the necessary functions.

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import patch
 import os
 import json
+from moto import mock_ses
 from utils.template import Template
 
 
@@ -30,7 +32,19 @@ class TestTemplate(unittest.TestCase):
         self.assertEqual(Template.create_or_update(self.sample_json), "Template 'Sample-Template' already exist, "
                                                                       "updated successfully")
 
-    def test_01_delete(self):
+    @mock_ses
+    def test_01_view(self):
+        with patch('utils.template.Template.ses.get_template') as mocked:
+            mocked.return_value.text = 'Template Fetched'
+            self.assertEqual(Template.view('Sample-Template').text, 'Template Fetched')
+        self.assertEqual(Template.view('New-Template'), 'Template does not exist')
+
+    @mock_ses
+    def test_02_list_all(self):
+        with patch('utils.template.Template.ses.list_templates') as mocked:
+            self.assertEqual(Template.list_all(), [])
+
+    def test_03_delete(self):
         self.assertEqual(Template.delete('Sample-Template'), 'Template deleted successfully')
 
 
